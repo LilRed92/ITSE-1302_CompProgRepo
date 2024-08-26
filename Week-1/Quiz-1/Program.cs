634ddb588abd0adf5064f2113630fce121188b1f
Kacie Dearman <deardevdesigner@gmail.com>
2024-08-26 07:58:43 -0500

    First commit for first quiz.

---
+++ Week-1/Quiz-1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+namespace QuizOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Quiz 1.");
+        }
+    }
+}
